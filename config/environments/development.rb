ea69a6a69f81d66eaa6b85eb94283f43d34710c7
gorotakeda <rugbytkg3@gmail.com>
2021-09-22 16:17:34 +0900

    gmailを送信する機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -61,4 +61,17 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
    # デバイスのメール設定
    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+   #Gメールの設定
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    enable_starttls_auto: true,
+    authentication: "plain",
+    user_name: 'rugbytkg3@gmail.com',
+    password: 'hyxyawtbcydcciep'
+  }
+
 end
